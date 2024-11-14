4d7309219937cf461ccd9a8fe45108cab2a208e0
Jimmy-Tempest <smartwattana@gmail.com>
2024-11-14 12:17:37 +0700

    feat: chat ui and evaluation

--- main.py
+++ main.py
@@ -1,24 +1,56 @@
-from random import choice
-
-from langchain_ollama import OllamaLLM, ChatOllama
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+import streamlit as st
 
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
-llm = ChatOllama(model="llama3.2"
-                #  , callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
-                 , temperature=0.9)
+from lib.models import get_chat_model, get_evaluation
 
 
-prompts = [
-    "You are a worried mother who is texting her son about his night out.",
-    "You are a friend who need a ride home from a party.",
-]
+def main():
+    llm, messages, role = get_chat_model()
 
-messages = [
-    SystemMessage(choice(prompts)),
-    HumanMessage("Hemlo ho us thixs?"),
-]
+    st.title(f"Chatting with {role[1]}")
 
-print((llm.invoke(messages)))
+    prompt = st.chat_input("You")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "system", "content": message.content} for message in messages
+        ]
+
+    for message in st.session_state.messages:
+        st.chat_message(message["role"]).markdown(message["content"])
+
+    if prompt:
+        st.chat_message("user").markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        response = llm(
+            [
+                (
+                    SystemMessage(message["content"])
+                    if message["role"] == "system"
+                    else (
+                        HumanMessage(message["content"])
+                        if message["role"] == "user"
+                        else AIMessage(message["content"])
+                    )
+                )
+                for message in st.session_state.messages
+            ]
+            + [HumanMessage(prompt)]
+        )
+        st.chat_message("ai").markdown(response.content)
+
+        st.session_state.messages.append({"role": "ai", "content": response.content})
+
+        result = get_evaluation(
+            [
+                f'{message["role"]}: {message["content"]}'
+                for message in st.session_state.messages
+            ]
+        )
+        st.session_state.evaluation = result.model_dump()
+        print(st.session_state.evaluation)
+
+
+if __name__ == "__main__":
+    main()
